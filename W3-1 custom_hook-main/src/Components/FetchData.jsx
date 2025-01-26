ca350125c963886e1114fd7be7f3f2a3e5a3be6e
Shin <basket0903@gmail.com>
2025-01-26 14:01:52 +0900

    Started W3 lab1 and did some work.

---
+++ W3-1 custom_hook-main/src/Components/FetchData.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import UseFetch from "./UseFetch";
+
+const FetchData = () => {
+  const [data] = UseFetch("https://api.npoint.io/9045c260b1565daa9e15");
+  console.log(data);
+  return (
+    <>
+     <ul className='list_data_main'>
+        <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
+        {data && data.map((e) => (
+          <li className="list_data">
+            <h3>{e.name}</h3>
+            <p></p>
+          </li>
+        ))}
+     </ul>
+    </>
+  )
+}
+
+export default FetchData
