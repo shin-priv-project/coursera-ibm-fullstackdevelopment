65d06ada1747bea4e2cb2e5245c341e7351cdb01
Shin <basket0903@gmail.com>
2025-02-27 22:37:27 +0900

    Half-finished lab W4-2 task3. Need to get back to this task.

--- W4-1/Coursera ReactJs lab W4-2 e-plantShopping-main/src/ProductList.jsx
+++ W4-1/Coursera ReactJs lab W4-2 e-plantShopping-main/src/ProductList.jsx
@@ -9,6 +9,8 @@ function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState([]);
+
+    const cart = useSelector(state => state.cart.items);
     const dispatch = useDispatch();
 
     const plantsArray = [
